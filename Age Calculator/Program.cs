70f5df156ebc2b8b037f777421abf7fd551b8aa6
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-06-05 00:42:26 +0500

    Add project files.

---
+++ Age Calculator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Birth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthYear, birthMonth, birthDay;
+            bool isValidInput = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Enter your birth year: ");
+                birthYear = int.Parse(Console.ReadLine());
+                if (birthYear <= 2023)
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a valid year.");
+                }
+            } while (!isValidInput);
+
+            isValidInput = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Enter your birth month: ");
+                if (int.TryParse(Console.ReadLine(), out birthMonth) && birthMonth >= 1 && birthMonth <= 12)
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a valid month (1-12).");
+                }
+            } while (!isValidInput);
+
+            isValidInput = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Enter your birth day: ");
+                if (int.TryParse(Console.ReadLine(), out birthDay) && birthDay >= 1 && birthDay <= 31)
+                {
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a valid day (1-31).");
+                }
+            } while (!isValidInput);
+
+            Console.WriteLine();
+
+            DateTime dateTime = new DateTime(birthYear, birthMonth, birthDay);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Your birth: {dateTime}");
+            Console.ResetColor();
+
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Now time: {now.Month}/{now.Day}/{now.Year}");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            TimeSpan elapsedTime = now - dateTime;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"You were born {elapsedTime.Days} days ago");
+            Console.ResetColor();
+        }
+    }
+}
